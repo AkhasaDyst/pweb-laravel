18b2a4b97955b2921cd38555061b156e268af9dc
Yudhi <yudhiwinantoro@gmail.com>
2023-11-24 14:10:11 +0700

    creating laravel app, with migrate and seeder and blade

--- blog-comp/resources/views/home.blade.php
+++ blog-comp/resources/views/home.blade.php
@@ -2,14 +2,22 @@
 <html>
 
     <head>
-        <title>Tugas Pengembangan Web</title>
+        <title>Welcome to Our Server</title>
     </head>
 
     <body>
-        <h2>Tugas Pengembangan Web</h2>
-        <p>Yudhi Winantoro</p>
-        <p>21060120120009</p>
-        <p>Teknik Elektro 2020</p>
+        <h2>Server 201</h2>
+        <p>Alif Racmat Ilahi 21060120120015</p>
+        <p>Arya Bramaputra 21060120120033</p>
+        <p>Alexander Caisar Simamora 21060120140185</p>
+        <p>Dani Sefianto 21060120120024</p>
+        <p>Fadhil Prawira 21060120140057</p>
+        <p>Guntur Awaludin Saptadi 21060120140088</p>
+        <p>Lukman Sanusi 21060120130140</p>
+        <p>Muhammad Diandra Junies Fahriza 21060120140159</p>
+        <p>Nadhif Lazuardi Aulia Rahim 21060120120004</p>
+        <p>Yudhi Winantoro 21060120120009</p>
+        <p>Hope youre Happy</p>
     </body>
 
 </html>
